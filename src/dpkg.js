ac91a9e3a551236dde414b7439ba8790256de3e4
taejin-hwang <taejin.hwang@lacework.net>
2021-06-30 21:05:31 -0700

    fixes to DPKG UDF

--- src/dpkg.js
+++ src/dpkg.js
@@ -1,4 +1,3 @@
-
 function parse(VER) {
   var version = /^([0-9]*(?=:))?:(.*)/.exec(VER);
   var v = {};
@@ -8,6 +7,10 @@ function parse(VER) {
   v.debian = (version.length>1)?version.pop():"";
   v.upstream = version.join("-");
   return v;
+}
+function truncateZeros(num) {
+  var truncated = num ? num.replace(/\b0+/, "") : "0";
+  return truncated ? truncated : "0";
 }
 function findIndex(ar,fn){
   for(var i = 0;i < ar.length; i++){
@@ -46,41 +49,57 @@ function compareChunk(a,b){
     return (charCode(ca[diff])>charCode(cb[diff]))?1:-1;
   }
 }
+function checkLast(matches, sign) {
+  if (matches == null) {
+    return 0;
+  } else {
+    var tail = truncateZeros(matches[1].split("")[0]);
+    if (tail === "0") {
+      return 0;
+    } else {
+      return -1 * sign;
+    }
+  }
+}
+
 function compareStrings(a,b){
-  if(a==b) return 0;
+  if(a===b) return 0;
   var parseA = /([^0-9]+|[0-9]+)/g;
   var parseB = /([^0-9]+|[0-9]+)/g;
   var ra,rb;
   while((ra=parseA.exec(a)) !== null && (rb = parseB.exec(b)) !== null ){
-    if((isNaN(ra[1]) || isNaN(rb[1])) && ra[1] != rb[1] ){
-      return compareChunk(ra[1],rb[1]);
+    if((isNaN(ra[1]) || isNaN(rb[1])) && ra[1] !== rb[1] ){
+      return ra[1] === rb[1] ? compareChunk(ra[1], rb[1]) : ra[1].localeCompare(rb[1])
     }else{ //both are numbers
-      if(ra[1]!=rb[1]){
+      ra[1] = truncateZeros(ra[1])
+      rb[1] = truncateZeros(rb[1])
+      if(ra[1]!==rb[1]){
         return (parseInt(ra[1])>parseInt(rb[1]))?1:-1;
       }
     }
   }
   if(!ra && rb){
-    return (parseB.exec(b)[1].split("")[0] == "~")?1:-1
+    return checkLast(parseB.exec(b), 1);
   }else if(ra && !rb){
-    return (ra[1].split("")[0] == "~")?-1:1
+    return checkLast(ra, -1);
   }else{
     return 0;
   }
 }
-function DPKG_COMPARE(S1, S2) {
-  var v1 = parse(S1);
-  var v2 = parse(S2);
+function DPKG_COMPARE(a, b){
+  var v1 = parse(a);
+  var v2 = parse(b);
+
   if (v1.upstream == "#MAXV#")
     return 1;
-  if ((v1.epoch > 0 || v2.epoch > 0) && Math.sign(v1.epoch - v2.epoch) != 0) {
-    return Math.sign(v1.epoch - v2.epoch);
+  if((v1.epoch>0 || v2.epoch>0) && Math.sign(v1.epoch-v2.epoch)!=0){
+    return Math.sign(v1.epoch-v2.epoch);
   }
-  if (compareStrings(v1.upstream, v2.upstream) != 0) {
-    return compareStrings(v1.upstream, v2.upstream);
-  } else {
-    return compareStrings(v1.debian, v2.debian);
+  if(compareStrings(v1.upstream,v2.upstream) !=0){
+    return compareStrings(v1.upstream,v2.upstream);
+  }else{
+    return compareStrings(v1.debian,v2.debian);
   }
-}
 
+}
 module.exports = DPKG_COMPARE;
